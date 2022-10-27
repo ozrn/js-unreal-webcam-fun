1e6d265b5d991d9ec8575862c9c35959265335ea
ozrn <ozerenkevser92@gmail.com>
2022-10-27 16:22:25 +0300

    Access user's camera

--- scripts.js
+++ scripts.js
@@ -3,3 +3,16 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+
+function getVideo(){
+  navigator.mediaDevices.getUserMedia({video: true, audio: false}) // this is a promise
+  .then(mediaStream => {
+    console.log(mediaStream);
+    video.srcObject = mediaStream;
+    video.play();
+  })
+  .catch(err => {
+    console.log("Access user camera has been disabled", err);
+  });
+}
+ getVideo();
